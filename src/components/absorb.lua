02173745980d2442926c5edd8817b798c8a88501
J. David Smith <emallson@archlinux.us>
2022-05-08 01:27:35 -0400

    absorb

---
+++ src/components/absorb.lua
@@ -0,0 +1,66 @@
+---@type LibSlab
+local Slab = LibStub('Slab')
+
+---@class AbsorbBarComponent:Component
+---@field public frame AbsorbBar
+local component = {
+    dependencies = {'healthBar'}
+}
+
+---@param slab Slab
+---@return AbsorbBar
+function component:build(slab)
+    local parent = slab.components.healthBar.frame
+    ---@class AbsorbBar:StatusBar
+    local absorb = CreateFrame('StatusBar', parent:GetName() .. 'AbsorbBar', parent)
+    absorb:SetAllPoints(parent)
+    absorb:SetStatusBarTexture('interface/raidframe/raid-bar-hp-fill')
+    absorb:SetStatusBarColor(243 / 255, 1, 115 / 255, 0.5)
+
+    absorb:Hide()
+
+    return absorb
+end
+
+---@param settings SlabNameplateSettings
+function component:bind(settings)
+    self.frame:RegisterUnitEvent("UNIT_MAXHEALTH", settings.tag)
+    self.frame:RegisterUnitEvent("UNIT_ABSORB_AMOUNT_CHANGED", settings.tag)
+end
+
+function component:unbind()
+    self.frame:UnregisterAllEvents()
+    self.frame:Hide()
+end
+
+function component:refreshMaxHp()
+    local max = UnitHealthMax(self.settings.tag)
+    self.frame:SetMinMaxValues(0, max)
+end
+
+function component:refreshAbsorb()
+    local absorb = UnitGetTotalAbsorbs(self.settings.tag)
+    self.frame:SetValue(absorb)
+    -- print(absorb)
+    if absorb > 0 then
+        self.frame:Show()
+    else
+        self.frame:Hide()
+    end
+end
+
+function component:update(eventName, ...)
+    if eventName == 'UNIT_MAXHEALTH' then
+        self:refreshMaxHp()
+    elseif eventName == 'UNIT_ABSORB_AMOUNT_CHANGED' then
+        self:refreshAbsorb()
+    end
+end
+
+---@param settings SlabNameplateSettings
+function component:refresh(settings)
+    self:refreshMaxHp()
+    self:refreshAbsorb()
+end
+
+Slab.RegisterComponent('absorb', component)
